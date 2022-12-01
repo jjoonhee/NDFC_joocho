396a541b70cdca923a0bfcacbba94f30ec347020
Joonhee Cho (joocho) <joocho@cisco.com>
2022-12-01 16:17:00 +0900

    minor commit

--- controller/conversation.py
+++ controller/conversation.py
@@ -11,7 +11,7 @@ def talk_about_API_action():
         ")
     while True:
         if select_method == "1":
-            pprint(f"Management IP =\n{obj_joocho.getSwitchMgmt()}")
+            pprint(f"Management IP = {obj_joocho.getSwitchMgmt()}")
             break
         elif select_method == "2":
             pprint(obj_joocho.getSwitchStatus())
