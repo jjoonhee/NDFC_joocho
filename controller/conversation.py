873c4828139cb2b0d85b7d900061335025a4288f
Joonhee Cho (joocho) <joocho@cisco.com>
2022-12-09 17:24:15 +0900

    minor commit

--- controller/conversation.py
+++ controller/conversation.py
@@ -5,12 +5,12 @@ from pprint import pprint
 def talk_about_API_action():
     obj_joocho = NDFC_api()
     while True:
-        select_method = input(f"{start_magenta}Select action you want:{end_color}\n\
-                1. Get Switches Management IP\n\
-                2. Get Switches status\n\
-                3. Get Alarms Info\n\
-                4. Delete Alarms\n\
-                5. Rediscover All switches\n Your input is : ")
+        select_method = input(f"""{start_magenta}Select action you want:{end_color}\n\
+            1. Get Switches Management IP\n\
+            2. Get Switches status\n\
+            3. Get Alarms Info\n\
+            4. Delete Alarms\n\
+            5. Rediscover All switches\n*Press 0(Zero) if you wanna close\n Your input is : """)
         if select_method == "1":
             pprint(f"Management IP = {obj_joocho.getSwitchMgmt()}")
             fin_var = 999
@@ -36,14 +36,17 @@ def talk_about_API_action():
         elif select_method == "5":
             print(obj_joocho.rediscoverSwitch()["resultMessage"])
             fin_var = 999
+        elif select_method == "0":
+            print("Closing...")
+            exit()
         else:
             print(f"{start_red}Wrong input. Try again{end_color}\n")
-            select_method = input(f"{start_magenta}Select action you want:{end_color}\n\
+            select_method = input(f"""{start_magenta}Select action you want:{end_color}\n\
                 1. Get Switches Management IP\n\
                 2. Get Switches status\n\
                 3. Get Alarms Info\n\
                 4. Delete Alarms\n\
-                5. Rediscover All switches\n Your input is : ")
+                5. Rediscover All switches\n*Press 0(Zero) if you wanna close\n Your input is : """)
         if fin_var == 999:
             fin_go = input("Wanna continue? Y/n").upper()
             if fin_go == "Y":
