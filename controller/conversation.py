15359041c38f0110451685a3f61c5290cc3e6a3c
Joonhee Cho (joocho) <joocho@cisco.com>
2022-12-09 16:43:22 +0900

    added PWD masking, Rediscovering feature

--- controller/conversation.py
+++ controller/conversation.py
@@ -4,34 +4,48 @@ from pprint import pprint
 
 def talk_about_API_action():
     obj_joocho = NDFC_api()
-    select_method = input(f"{start_magenta}Select action you want:{end_color}\n\
-            1. Get Switches Management IP\n\
-            2. Get Switches status\n\
-            3. Get Alarms Info\n\
-            4. Delete Alarms\nYour input is : \
-        ")
     while True:
+        select_method = input(f"{start_magenta}Select action you want:{end_color}\n\
+                1. Get Switches Management IP\n\
+                2. Get Switches status\n\
+                3. Get Alarms Info\n\
+                4. Delete Alarms\n\
+                5. Rediscover All switches\n Your input is : \
+            ")
         if select_method == "1":
             pprint(f"Management IP = {obj_joocho.getSwitchMgmt()}")
-            break
+            select_method == "999"
         elif select_method == "2":
-            pprint(obj_joocho.getSwitchStatus())
-            break
+            pprint(obj_joocho.getSwitchStatus()[0])
+            select_method == "999"
         elif select_method == "3":
             obj_joocho.returnAlmMsg()
-            clear_var = input(f"{start_magenta}Do you want clear the alarms? Y/n{end_color} ").upper()
+            clear_var = input(f"\n{start_magenta}Do you want clear the alarms? Y/n{end_color} ").upper()
             while True:
                 if clear_var == "Y":
                     obj_joocho.deleteAlarms()
-                    break
+                    select_method == "999"
                 elif clear_var == "N":
+                    select_method == "999"
                     break
                 else:
                     print(f"{start_red}Wrong Input please input Y or N{end_color}")
                     clear_var = input(f"{start_magenta}Do you want clear the alarms? Y/n{end_color} ").upper()
-            break
         elif select_method == "4":
             pprint(obj_joocho.deleteAlarms())
+            select_method == "999"
+        elif select_method == "5":
+            print(obj_joocho.rediscoverSwitch()["resultMessage"])
+            select_method == "999"
+        elif select_method == "999":
+            fin_var = input("Wanna continue? Y/n").upper()
+            if fin_var == "Y":
+                continue
+            elif fin_var == "N":
+                break
+            else:
+                print("Wrong Input, Closing...")
+                exit()
         else:
             print(f"{start_red}Wrong input. Try again{end_color}\n")
             select_method = input("""Select action you want:
--- model/handler.py
+++ model/handler.py
@@ -2,18 +2,32 @@ import requests
 import os
 import json
 import pprint as pprint
+import time
+import sys
+import subprocess
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 start_magenta = "\033[95m"
 start_red = "\033[31m"
 end_color = "\033[0m"
-update_date = "Last Updated date : 2022.12.1, joocho"
+update_date = "Last Updated date : 2022.12.9, joocho"
+
+try:
+    import pwinput
+except:
+    subprocess.check_call([sys.executable, "pip", "install", "pwinput"])
+    import pwinput
+try:
+    from tqdm import tqdm
+except:
+    subprocess.check_call([sys.executable, "pip", "install", "tqdm"])
+    from tqdm import tqdm
 
 class Login_NDFC():
     __doc__="Hello"
     def loginNDFC(self):
         self.userName = input("Insert your username: ")
-        self.passWd = input("Input your password: ")
+        self.passWd = pwinput.pwinput("Input your password: ", mask="*")
         while True:
             try:
                 self.payload = json.dumps({
@@ -31,7 +45,7 @@ class Login_NDFC():
             except:
                 print(f"{start_red}Authentication failed, Check your username/Password{end_color}")
                 self.userName = input("Insert your username: ")
-                self.passWd = input("Insert your password: ")
+                self.passWd = pwinput.pwinput("Insert your password: ")
     
 
 class NDFC_api(Login_NDFC):
@@ -80,13 +94,14 @@ class NDFC_api(Login_NDFC):
         sum_status = {}
         url = f"https://{self.login_input}/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/inventory/allswitches"
         response = requests.request("GET", url, headers = self.headers, data = self.payload, verify = False)
-        res = json.loads(response.text)
-        for k in range(len(res)):
-            sum_status[res[k]["logicalName"]] = {"ccStatus" : "", "fabricName" : "", "ipAddress": ""}
-            sum_status[res[k]["logicalName"]]["ccStatus"] = res[k]["ccStatus"]
-            sum_status[res[k]["logicalName"]]["fabricName"] = res[k]["fabricName"]
-            sum_status[res[k]["logicalName"]]["ipAddress"] = res[k]["ipAddress"]
-        return sum_status
+        self.res = json.loads(response.text)
+        for k in range(len(self.res)):
+            sum_status[self.res[k]["logicalName"]] = {"ccStatus" : "", "fabricName" : "", "ipAddress": "", "Discover_status": ""}
+            sum_status[self.res[k]["logicalName"]]["ccStatus"] = self.res[k]["ccStatus"]
+            sum_status[self.res[k]["logicalName"]]["fabricName"] = self.res[k]["fabricName"]
+            sum_status[self.res[k]["logicalName"]]["ipAddress"] = self.res[k]["ipAddress"]
+            sum_status[self.res[k]["logicalName"]]["Discover_status"] = self.res[k]["status"]
+        return sum_status, self.res
 
 
     def getAlarmInfo(self):
@@ -109,7 +124,6 @@ class NDFC_api(Login_NDFC):
         #print("*"*20 + str(len(self.res_result)) + "*"*20)
         if id_list == []:
             print("There is no alarm.\nClosing...")
-            exit()
         return id_list, self.res_result
 
 
@@ -121,21 +135,49 @@ class NDFC_api(Login_NDFC):
             print("DELETE Excution complete.")
         if self.getAlarmInfo()[0] != []:
             self.deleteAlarms()
-        exit()
 
     def returnAlmMsg(self):
         total_alm_count = 0
         alarm_ID, res_contents = self.getAlarmInfo()
         inner_list = []
         msg_dict = {"Info" : inner_list}
-        for idx in range(len(res_contents)):
+        for idx in tqdm(range(len(res_contents))):
             for msg in res_contents[idx]:
                 inner_list.append("Device = " + msg["deviceName"] + ", Message = " + msg["message"])
         for idx, val in enumerate(msg_dict["Info"]):
             print(f"Index: {idx+1}\n -> {val}")
             total_alm_count += 1
         print(f"{start_magenta}Total Alarm Count = {total_alm_count}\nTotal API call time = {self.call_time:.3f} seconds.{end_color}")
+    
+    def rediscoverSwitch(self):
+        sw_payload = self.getSwitchStatus()[1]
+        payload = []
+        url = f"https://{self.login_input}/appcenter/cisco/ndfc/api/v1/lan-discovery/rediscoverSwitch"
+        for k in range(len(sw_payload)):
+            payload.append(sw_payload[k]["switchDbID"])
+        response = requests.request("POST", url, headers= self.headers, data= str(payload), verify = False)
+        response = json.loads(response.text)
+        if response["resultMessage"] == "OK":
+            for _ in tqdm(range(15)):
+                time.sleep(1)
+            while True:
+                sw_status = self.getSwitchStatus()[1]
+                tmp_list = set()
+                for sts in range(len(sw_status)):
+                    tmp_list.add(sw_status[sts]["status"])
+                tmp_list = list(tmp_list)    
+                if (len(tmp_list) == 1 and ("Rediscovering" not in tmp_list) and ("ok" in tmp_list)):
+                    print("All swiches are discovered")
+                    for k,v in self.getSwitchStatus()[0].items():
+                        print(k,"=",v["Discover_status"])
+                    return response
+                elif ("Rediscovering" in tmp_list):
+                    print("Discovering...")
+                else:
+                    print("Check the switch connectivity\nClosing...")
+                    break
+        
 
 
 #test = NDFC_api()
-#test.getSwitchStatus()
+#print(test.getSwitchStatus()[0])
