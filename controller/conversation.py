6c591f5879227e5a7ea9456a9675c81f0265472d
Joonhee Cho (joocho) <joocho@cisco.com>
2022-12-13 18:07:35 +0900

    minor commit

--- controller/conversation.py
+++ controller/conversation.py
@@ -4,6 +4,7 @@ from pprint import pprint
 
 def talk_about_API_action():
     obj_joocho = NDFC_api()
+    fin_var = False
     while True:
         select_method = input(f"""{start_magenta}Select action you want:{end_color}\n\
             1. Get Switches Management IP\n\
@@ -13,32 +14,32 @@ def talk_about_API_action():
             5. Rediscover All switches\n*Press 0(Zero) if you wanna close\n Your input is : """)
         if select_method == "1":
             pprint(f"Management IP = {obj_joocho.getSwitchMgmt()}", indent = 4)
-            fin_var = 999
+            fin_var = True
         elif select_method == "2":
             pprint(obj_joocho.getSwitchStatus()[0])
-            fin_var = 999
+            fin_var = True
         elif select_method == "3":
             if bool(obj_joocho.returnAlmMsg()) is False:
-                fin_var = 999
+                fin_var = True
             else:
                 clear_var = input(f"\n{start_magenta}Do you want to clear the alarms? Y/n{end_color} ").upper()
                 while True:
                     if clear_var == "Y":
                         obj_joocho.deleteAlarms()
-                        fin_var = 999
+                        fin_var = True
                         break
                     elif clear_var == "N":
-                        fin_var = 999
+                        fin_var = True
                         break
                     else:
                         print(f"{start_red}Wrong Input please input Y or N{end_color}")
                         clear_var = input(f"{start_magenta}Do you want to clear the alarms? Y/n{end_color} ").upper()
         elif select_method == "4":
             obj_joocho.deleteAlarms()
-            fin_var = 999
+            fin_var = True
         elif select_method == "5":
             print(obj_joocho.rediscoverSwitch()["resultMessage"])
-            fin_var = 999
+            fin_var = True
         elif select_method == "0":
             print("Closing...")
             exit()
@@ -50,7 +51,7 @@ def talk_about_API_action():
                 3. Get Alarms Info\n\
                 4. Delete Alarms\n\
                 5. Rediscover All switches\n\n*Press 0(Zero) if you wanna close\n Your input is : """)
-        if fin_var == 999:
+        if fin_var == True:
             while True:
                 fin_go = input("Wanna continue? Y/n ").upper()
                 if fin_go == "Y":
